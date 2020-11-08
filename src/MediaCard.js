d25260859e45d20b957d1be07558cae2d6989da2
MuhammadAbdullah9 <Abdullahimtiaz549@gmail.com>
2020-11-08 14:43:16 +0500

    Updated alt error of image

--- src/MediaCard.js
+++ src/MediaCard.js
@@ -8,7 +8,7 @@ function MediaCard(props) {
       <hr id="short" />
       <h2>{props.title}</h2>
       <p className="bold">{props.body}</p>
-      <img src={props.imageUrl} />
+      <img src={props.imageUrl} alt="react-logo"/>
 <small><p>The other two are text while image not, we can return them at once but then text would be showing at image place</p></small>
 <hr id="short" />
 <Gate />
